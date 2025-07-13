4da0f178e94dcd313f2cd462b91672a4b6c976f9
AlanJake <alanjake12138@gmail.com>
2025-07-13 10:47:21 +0800

    业务-销量排行Top10代码实现

--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -1,10 +1,12 @@
 package com.sky.service;
 
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * ClassName:ReportService
@@ -21,4 +23,6 @@ public interface ReportService {
     UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 
     OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end);
+
+    SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
 }
